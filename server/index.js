65d32cc8f87705d26714e8f58932d2207f5176d9
Aryanoo1 <96529828+Aryanoo1@users.noreply.github.com>
2024-10-08 03:02:05 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -4,12 +4,22 @@ import cors from "cors";
 import db from "./config/db.js";
 import session from "express-session";
 import MongoStore from "connect-mongo";
+import helmet from "helmet";
 import UserModel from "./models/UserModel.js";
 import BlogModel from "./models/BlogModel.js";
 import router from "./routes/index.js";
 
 const app = express();
 dotenv.config();
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://vercel.live"],
+    },
+  })
+);
 
 app.use(express.json());
 app.use(
@@ -38,11 +48,11 @@ app.use(
 );
 
 app.use("/api", router);
-app.get('/',(request,response)=>{
+app.get("/", (request, response) => {
   response.json({
-      message : "Server running at " + PORT
-  })
-})
+    message: "Server running at " + PORT,
+  });
+});
 
 db().then(() => {
   app.listen(8080, () => {
