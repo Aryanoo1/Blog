8f20f87682714d1eecaff335e38ecb2382a4f9a9
Aryanoo1 <96529828+Aryanoo1@users.noreply.github.com>
2024-10-08 19:56:46 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -37,7 +37,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const storage = new Storage({
-  keyFilename: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON),
+  keyFilename: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS),
 });
 
 export const bucket = storage.bucket(process.env.GCS_BUCKET_NAME);
